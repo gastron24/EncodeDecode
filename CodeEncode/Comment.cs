48ea3571f00acd4dfd086f924e56d1b604257155
gastron24 <pizdatez@gmail.com>
2025-05-20 14:38:26 +0300

    EncodeDecode

---
+++ CodeEncode/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEncode
+{
+    class Comment
+    {
+        /// потратил 2,5 - 3 часа, без ии не обошлось(самые крайние моменты, коркас вроде сам смастерил)
+        /// p.s вышел на перекур раз 8
+
+
+
+    }
+}
---
+++ CodeEncode/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace CodeEncode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите действие: ");
+            Console.WriteLine("1 - Зашифроват файл в base64:");
+            Console.WriteLine("2 - Расшифровать base64 в файл");
+
+            int InputChoice = int.Parse(Console.ReadLine());
+
+            switch(InputChoice)
+            {
+                case 1:
+                    EncodeFileToBase();
+                    break;
+                case 2:
+                    DecodeBaseToFile();
+                    break;
+               
+                default: Console.WriteLine("Некоректный ввод");
+                    break;
+
+            }
+
+            static void EncodeFileToBase()
+            {
+                Console.Write("Введите полный путь к файлу для кодирования");
+                string InputFilePatch = Console.ReadLine();
+
+                if(!File.Exists(InputFilePatch))
+                {
+                    Console.WriteLine("файл не найден...");
+                }
+                byte[] bytes = File.ReadAllBytes(InputFilePatch);
+                string base64string = Convert.ToBase64String(bytes);
+
+                string outputFilePath = Path.ChangeExtension(InputFilePatch, "Base64.txt");
+                File.WriteAllText(outputFilePath, base64string);
+                Console.WriteLine($"Строка сохранена в {outputFilePath} ");
+            }
+
+            static void DecodeBaseToFile()
+            {
+                Console.WriteLine("Введите путь куда сохранить файл");
+                string outputFilePatch = Console.ReadLine();
+
+                Console.WriteLine("Введите base64 строку");
+                string? inputBasePatch = Console.ReadLine();
+
+                try
+                {
+                    byte[] fileBytes = Convert.FromBase64String(inputBasePatch);
+                    File.WriteAllBytes(outputFilePatch, fileBytes);
+                    Console.WriteLine($"Файл успешно восстановлен - {outputFilePatch} ");
+                }
+                catch 
+                {
+                    Console.WriteLine("Ошибка ввода");
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
